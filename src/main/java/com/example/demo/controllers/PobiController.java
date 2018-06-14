d4698a3491289ee0f00c4cd255f78cc9268cbad0
saru <sarguments@gmail.com>
2018-06-15 00:36:27 +0900

    좀더 일반적으로, 리팩토링

--- src/main/java/com/example/demo/controllers/PobiController.java
+++ src/main/java/com/example/demo/controllers/PobiController.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.cloud.translate.Translate;
 import com.google.cloud.translate.TranslateOptions;
 import com.google.cloud.translate.Translation;
+import com.google.common.collect.Lists;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,10 +21,17 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 public class PobiController {
+    private static final String CONTENT = "content";
+    private static final String CHAT = "chat";
+    private static final String COMMENT_SERVER_URL = "http://pobi.god/api.json";
     private static final Logger log = LoggerFactory.getLogger(PobiController.class);
+    private static final String JAPAN = "ja";
+    private static final String KOREA = "ko";
+    private static final int SPLIT_SIZE = 100;
     private static Translate translate = TranslateOptions.newBuilder().build().getService();
     private ObjectMapper mapper = new ObjectMapper();
 
@@ -34,122 +42,162 @@ public class PobiController {
         return "Hello";
     }
 
-    private static Translation translateTextWithOptions(String sourceText) {
-        Translate.TranslateOption srcLang = Translate.TranslateOption.sourceLanguage("ja");
-        Translate.TranslateOption tgtLang = Translate.TranslateOption.targetLanguage("ko");
-        return translate.translate(sourceText, srcLang, tgtLang);
-    }
+    private static List<Translation> translateList(List<String> sourceTexts) {
+        Translate.TranslateOption srcLang = Translate.TranslateOption.sourceLanguage(JAPAN);
+        Translate.TranslateOption tgtLang = Translate.TranslateOption.targetLanguage(KOREA);
 
-    private static List<Translation> translationsList(List<String> sourceTexts) {
-        Translate.TranslateOption srcLang = Translate.TranslateOption.sourceLanguage("ja");
-        Translate.TranslateOption tgtLang = Translate.TranslateOption.targetLanguage("ko");
-        return translate.translate(sourceTexts, srcLang, tgtLang);
+        // GUAVA 활용해서 100개 단위로 나눈다
+        List<List<String>> listOfList = Lists.partition(sourceTexts, SPLIT_SIZE);
+
+        // 나눠진 리스트를 스트림으로 각각 번역 후 합쳐서 리스트로 리턴
+        return listOfList.stream().map(list -> translate.translate(list, srcLang, tgtLang))
+                .flatMap(List::stream).collect(Collectors.toList());
     }
 
     @PostMapping("/api.json")
     public ResponseEntity<String> postWelcome(
             @RequestHeader HttpHeaders httpHeaders,
-            @RequestBody String json) throws IOException {
+            @RequestBody String json) {
         Set<Map.Entry<String, List<String>>> headers = httpHeaders.entrySet();
         for (Map.Entry<String, List<String>> header : headers) {
             log.info("key : value = {} : {}", header.getKey(), header.getValue());
         }
 
-        // 요청 헤더 생성
-        HttpHeaders makeHeaders = new HttpHeaders();
-        makeHeaders.set("accept", "*/*");
-        makeHeaders.set("accept-encoding", "gzip, deflate");
-        makeHeaders.set("accept-language", "ko,en-US;q=0.9,en;q=0.8,it-IT;q=0.7,it;q=0.6,ja;q=0.5");
-        makeHeaders.set("cache-Control", "no-cache");
-        makeHeaders.set("connection", "keep-alive");
-        makeHeaders.set("content-type", "text/plain;charset=UTF-8");
-        makeHeaders.set("host", "nmsg.nicovideo.jp");
-        makeHeaders.set("origin", "http://www.nicovideo.jp");
-        makeHeaders.set("pragma", "no-cache");
-        makeHeaders.set("referer", "http://www.nicovideo.jp/watch/sm5361236");
-        makeHeaders.set("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.62 Safari/537.36");
-
-        HttpEntity<String> httpEntity = new HttpEntity<>(json, makeHeaders);
+        // 요청 헤더 생성 후 json과 같이 httpEntity 조합
+        HttpEntity<String> httpEntity = new HttpEntity<>(json, makeHeaders(httpHeaders));
 
         // gzip으로 인해 아파치 라이브러리 사용 - HttpComponentsClientHttpRequestFactory
-        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(
-                HttpClientBuilder.create().build());
-        RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory);
-
-        // content type 설정
-        HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.set("Content-Type", "text/json; charset=UTF-8");
+        RestTemplate restTemplate = makeRestTemplate();
 
         // json 얻어온다
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity("http://pobi.god/api.json", httpEntity, String.class);
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(COMMENT_SERVER_URL, httpEntity, String.class);
         log.info(responseEntity.getBody());
 
         //-------------------------------------------------------------------------------
 
-        // json to Object
+        // json to Items
+        List<Item> items = responseJsonToItems(responseEntity);
+
+        // 번역할 텍스트의 원본 Content 참조와 텍스트 pairs로 추출
+        List<Pair> pairs = itemsToPairs(items);
+        log.info("pairList size : {}", pairs.size());
+
+        // pairs에서 번역할 텍스트만 따로 추출
+        List<String> toTransTexts = extractToTranslateTexts(pairs);
+        log.info("toTransList size : {}", toTransTexts.size());
+
+        // 추출한 리스트 번역
+        List<Translation> translatedTexts = translateList(toTransTexts);
+        log.info("translatedList size : {}", translatedTexts.size());
+
+        // 번역 텍스트 다시 put
+        putTranslatedTexts(pairs, translatedTexts);
+
+        // 테스트 번역 텍스트 출력
+        printTranslateText(items);
+
+        //-------------------------------------------------------------------------------
+
+        // object To Json
+        String translatedJson = itemsToJson(items);
+
+        // 테스트 json 출력
+        log.info(translatedJson);
+
+        // 응답 content type 설정 후 브라우저로 ResponseEntity 리턴
+        return new ResponseEntity<>(translatedJson, makeResponseHeaders(), HttpStatus.OK);
+    }
+
+    private HttpHeaders makeResponseHeaders() {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Content-Type", "text/json; charset=UTF-8");
+        return responseHeaders;
+    }
+
+    private RestTemplate makeRestTemplate() {
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(
+                HttpClientBuilder.create().build());
+        return new RestTemplate(clientHttpRequestFactory);
+    }
+
+    private void printTranslateText(List<Item> items) {
+        for (Item item : items) {
+            Optional.ofNullable(item.get(CHAT)).ifPresent(chat -> log.info((String) chat.get(CONTENT)));
+        }
+    }
+
+    private HttpHeaders makeHeaders(HttpHeaders httpHeaders) {
+        HttpHeaders toRequestHeaders = new HttpHeaders();
+
+        toRequestHeaders.setAccept(httpHeaders.getAccept());
+        toRequestHeaders.set(HttpHeaders.ACCEPT_ENCODING, httpHeaders.get(HttpHeaders.ACCEPT_ENCODING).toString());
+        toRequestHeaders.setAcceptLanguage(httpHeaders.getAcceptLanguage());
+        toRequestHeaders.setCacheControl(httpHeaders.getCacheControl());
+        toRequestHeaders.setConnection(httpHeaders.getConnection());
+        toRequestHeaders.setContentType(httpHeaders.getContentType());
+        toRequestHeaders.setHost(httpHeaders.getHost());
+        toRequestHeaders.setOrigin(httpHeaders.getOrigin());
+        toRequestHeaders.setPragma(httpHeaders.getPragma());
+        toRequestHeaders.set(HttpHeaders.REFERER, httpHeaders.get(HttpHeaders.REFERER).toString());
+        toRequestHeaders.set(HttpHeaders.USER_AGENT, httpHeaders.get(HttpHeaders.USER_AGENT).toString());
+
+        return toRequestHeaders;
+    }
+
+    private String itemsToJson(List<Item> items) {
+        String translatedJson = null;
+
+        try {
+            translatedJson = mapper.writeValueAsString(items);
+        } catch (JsonProcessingException e) {
+            log.info(e.getMessage());
+        }
+        return translatedJson;
+    }
+
+    private List<Item> responseJsonToItems(ResponseEntity<String> responseEntity) {
         List<Item> items = null;
+
         try {
             items = mapper.readValue(responseEntity.getBody(),
                     new TypeReference<List<Item>>() {
                     });
         } catch (IOException e) {
-            e.printStackTrace();
+            log.info(e.getMessage());
         }
+        return items;
+    }
 
-        // TODO 일단 100개만 번역
+    private List<Pair> itemsToPairs(List<Item> items) {
         List<Pair> pairList = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            Optional.ofNullable(items.get(i).findContents()).ifPresent(pairList::add);
+        for (Item item : items) {
+            Optional.ofNullable(item.findContents()).ifPresent(pairList::add);
+            log.info("find content : {}", item);
         }
 
-        // 번역할 텍스트 추출
+        return pairList;
+    }
+
+    private List<String> extractToTranslateTexts(List<Pair> pairList) {
         List<String> toTransList = new ArrayList<>();
         for (Pair pair : pairList) {
             toTransList.add(pair.getMsg());
         }
 
-        // 리스트 번역
-        List<Translation> translatedList = translationsList(toTransList);
+        return toTransList;
+    }
 
-        // 번역 텍스트 put
+    private void putTranslatedTexts(List<Pair> pairList, List<Translation> translatedList) {
         for (int i = 0; i < pairList.size(); i++) {
-            pairList.get(i).getContents().put("content", translatedList.get(i).getTranslatedText());
+            pairList.get(i).getContents().put(CONTENT, translatedList.get(i).getTranslatedText());
         }
-
-        // 테스트 번역 텍스트 출력
-        for (Item item : items) {
-            Optional.ofNullable(item.get("chat")).ifPresent(chat -> System.out.println(chat.get("content")));
-        }
-
-        //-------------------------------------------------------------------------------
-
-        // object To Json
-        String tranedJson = null;
-        try {
-            tranedJson = mapper.writeValueAsString(items);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-
-        // 테스트 json 출력
-        System.out.println(tranedJson);
-
-        // 브라우저로 ResponseEntity 리턴
-        return new ResponseEntity<>(tranedJson, responseHeaders, HttpStatus.OK);
     }
 
     private static class Contents extends HashMap<String, Object> {
         Pair findContent() {
             for (String s : keySet()) {
-                if (s.equals("content")) {
-                    String originalText = (String) get("content");
-
-//                    Object obj = this;
-
-//                    log.info("content : {}", originalText);
-//                    Translation translation = translateTextWithOptions(originalText);
-//                    put("content", translation.getTranslatedText());
-
+                if (s.equals(CONTENT)) {
+                    String originalText = (String) get(CONTENT);
                     return new Pair(this, originalText);
                 }
             }
@@ -162,9 +210,8 @@ public class PobiController {
         Pair findContents() {
             Set<Map.Entry<String, Contents>> itemSets = entrySet();
             for (Map.Entry<String, Contents> itemSet : itemSets) {
-                if (itemSet.getKey().equals("chat")) {
+                if (itemSet.getKey().equals(CHAT)) {
                     Contents contents = itemSet.getValue();
-                    log.info("item find");
                     return contents.findContent();
                 }
             }
