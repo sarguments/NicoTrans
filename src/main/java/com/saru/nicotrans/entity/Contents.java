0c50b52b944d79e7a8466f1441d523e716983599
saru <sarguments@gmail.com>
2018-08-17 15:46:50 +0900

    for문 stream 으로 대체

--- src/main/java/com/saru/nicotrans/entity/Contents.java
+++ src/main/java/com/saru/nicotrans/entity/Contents.java
@@ -6,13 +6,10 @@ import static com.saru.nicotrans.typeAndConfig.ContentType.CONTENT;
 
 public class Contents extends HashMap<String, Object> {
     Pair findContent() {
-        for (String s : keySet()) {
-            if (s.equals(CONTENT.getName())) {
-                return new Pair(this, getContentString());
-            }
-        }
-
-        return null;
+        // 키셋을 돌면서 "CONTENT" 를 찾은다음, "CONTENT" 의 레퍼런스와 코멘트 텍스트 리턴
+        return keySet().stream()
+                .filter(s -> s.equals(CONTENT.getName()))
+                .map(c -> new Pair(this, c)).findFirst().orElse(null);
     }
 
     String getContentString() {
